8ac8b15dc82406984d0fd92f33a659ba189d47f0
Subham Mitra <subham.mitra21@gmail.com>
2021-01-22 15:09:45 +0000

    improved indentation and spacing

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 const readLineSync = require('readline-sync');
 
 const name = readLineSync.question('Hi,I am Quizee.I am a very interactive quiz application.So,what is your name?\n');
+
 console.log(`\nHello ${name}, welcome to Quizee !! Good to see you.Lets begin..\n`);
 
  const questions=[
    {
-   question:'Who did England beat in the final of the 2019 Cricket World Cup?',
-   options:['India','New Zealand','Australia','South Africa'],
-   answer:2
+   question : 'Who did England beat in the final of the 2019 Cricket World Cup?',
+   options : ['India','New Zealand','Australia','South Africa'],
+   answer : 2
    },
- {
-   question:'What was the first Disney film that was produced in color?',
-   options:['Cinderella','Snow White and the Seven Dwarfs','Sleeping Beauty','Pocahontas'],
-   answer:4
- },
- {
-question:'What is the name of the company that published the Mario Kart video game?',
-options:['Nintendo','Electronic Arts (EA)','SEGA','Xbox'],
-answer:1
- },
- {
-   question:'How many legs does a spider have?',
-   options:[1,2,4,8],
-   answer:4
- },
- {
-   question:'What was the name of Harry Potter’s pet owl?',
-   options:['Dobby','Nimbus','Hedwig','Fenwick'],
-   answer:3
- }];
+   {
+   question : 'What was the first Disney film that was produced in color?',
+   options : ['Cinderella','Snow White and the Seven Dwarfs','Sleeping Beauty','Pocahontas'],
+   answer : 2
+   },
+   {
+   question : 'What is the name of the company that published the Mario Kart video game?',
+   options : ['Nintendo','Electronic Arts (EA)','SEGA','Xbox'],
+   answer : 1
+   },
+   {
+   question : 'How many eyes does a spider have?',
+   options : [1,2,4,8],
+   answer : 4
+   },
+   {
+   question : 'What was the name of Harry Potter’s pet owl?',
+   options : ['Dobby','Nimbus','Hedwig','Fenwick'],
+   answer : 3
+   }];
 
-let points=logic();
+let points = logic();
 console.log(displayResults(points));
 
 function displayQuestions(i)
@@ -40,53 +41,53 @@ function displayQuestions(i)
 
 function displayOptions(i)
 {
-  for(let optionIndex=0;optionIndex<4;optionIndex++)
+   for(let optionIndex=0;optionIndex<4;optionIndex++)
   {
-   console.log(`${optionIndex+1}. ${questions[i].options[optionIndex]}`);
+     console.log(`${optionIndex+1}. ${questions[i].options[optionIndex]}`);
   }
 }
 
-function validateResponse(user_response)
+function validateResponse(userResponse)
 {
-if(user_response==1 || user_response==2 || user_response==3 ||user_response==4)
-return true;
-else
-return false;
+   if(userResponse==1 || userResponse==2 || userResponse==3 || userResponse==4)
+     return true;
+   else
+     return false;
 }
+
 function logic()
 {
-  let score=0;
-  for(let i=0;i<questions.length;i++)
-  {
-   
-    displayQuestions(i);
-    displayOptions(i);
-    const user_response = readLineSync.question('\nEnter your option: ');
-   if(validateResponse(user_response))
+   let score=0;
+   for(let i=0;i<questions.length;i++)
    {
-      if(user_response==questions[i].answer)
-    {
-       console.log('Correct Answer\n');
-       score+=1;
-    }
-    else
-    {
-      console.log('Wrong Answer');
-      console.log(`Your answer: ${questions[i].options[user_response-1]} , Correct answer: ${questions[i].options[questions[i].answer-1]}\n`);
-    }
-    }
-    else
-    {
-      console.log('\nPlease enter valid option (1/2/3/4) next time.\n');
-    }
+     displayQuestions(i);
+     displayOptions(i);
+     const userResponse = readLineSync.question('\nEnter your option: ');
+     if(validateResponse(userResponse))
+     {
+       if(userResponse==questions[i].answer)
+         {
+           console.log('Correct Answer\n');
+           score+=1;
+         }
+       else
+         {    
+           console.log('Wrong Answer');
+           console.log(`Your answer: ${questions[i].options[userResponse-1]} , Correct answer: ${questions[i].options[questions[i].answer-1]}\n`);
+         }
+      }
+      else
+       {
+         console.log('\nPlease enter valid option (1/2/3/4) next time.\n');
+       }
   }
-  return score;
+return score;
 }
 
-  function displayResults(points)
-  {
-    console.log(`Quiz over.Congrats ! You have scored ${points} out of ${questions.length}.`);
-  }
+function displayResults(points)
+{
+   console.log(`Quiz over.Congrats ! You have scored ${points} out of ${questions.length}.`);
+}
   
  
 
